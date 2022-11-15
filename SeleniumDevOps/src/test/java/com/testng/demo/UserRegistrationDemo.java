b961f422cced1bb3750d9df594cfbce640beb19f
Kamran <112368797+KamranRaza12@users.noreply.github.com>
2022-11-15 11:19:54 +0530

    checkupdate

--- SeleniumDevOps/src/test/java/com/testng/demo/UserRegistrationDemo.java
+++ SeleniumDevOps/src/test/java/com/testng/demo/UserRegistrationDemo.java
@@ -17,7 +17,7 @@ public class UserRegistrationDemo {
 	public WebDriver driver;
 
 	// Parameterizing all WebElement paths
-	
+	//check jenkins
 
 	String titleLocator = "//*[text()='Register For Demo']";
 	String firstNameLocator = "vfb-5";
